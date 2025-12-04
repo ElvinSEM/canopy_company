ad0cda06a366be429196b209d73f59ab74eb0a87
Elvin <elvin.seytibramov@gmail.com>
2025-12-04 21:59:08 +0300

    fix: configure allowed hosts from ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.hosts << ENV['RAILS_HOSTS'] if ENV['RAILS_HOSTS'].present?
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
